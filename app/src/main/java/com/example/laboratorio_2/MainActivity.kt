3da2d5c6cd6ff06171f6016baf82818b0ab4f12f
IvanaFD <ivanafigueroa0936@gmail.com>
2025-08-01 01:41:23 -0600

    Better structure of hello world with image

--- app/src/main/java/com/example/laboratorio_2/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio_2/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.laboratorio_2.ui.theme.Laboratorio_2Theme
 
@@ -18,6 +21,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent{
+            MyImage()
             MyTexts()
             //Esta funcion composable se encarga de mostrar en pantalla el texto
         }
@@ -25,12 +29,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+@Composable
+fun MyImage(){
+    Image(
+        painterResource(R.drawable.ic_launcher_foreground),"Imagen de prueba")
+
+}
+
 //Esta funcion contruye la interfaz de usuario
 @Composable
 fun MyTexts(){
-    //Dentro usa otra funcion que permite mostrar el texto
-    MyText("Hola Mundo")
-    MyText("Prueba de Compose")
+    Column() {
+        //Dentro usa otra funcion que permite mostrar el texto
+        MyText("Hola Mundo")
+        MyText("Prueba de Compose")
+    }
 }
 //Esta funci[on permite que sea reutilizable para mostrar texto
 @Composable
@@ -43,5 +58,6 @@ fun MyText(text: String){
 @Preview
 @Composable
 fun PreviewText(){
+    MyImage()
     MyTexts()
 }
